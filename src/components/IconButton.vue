e3a9eede734a232598938421eb8bad31ed359d8b
Rui <75087533+chenrui1124@users.noreply.github.com>
2025-09-01 23:14:40 +0800

    Add component & layout

---
+++ src/components/IconButton.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+defineProps<{
+  icon: string
+}>()
+
+defineEmits<{
+  click: [evt: MouseEvent]
+}>()
+</script>
+
+<template>
+  <button
+    @click="$emit('click', $event)"
+    class="preset-outline ml-auto inline-flex size-9 cursor-pointer items-center justify-center rounded-full bg-pri/90 text-on-pri drop-shadow-md backdrop-blur-md hover:bg-pri"
+  >
+    <span aria-hidden="true" class="pointer-events-none size-4.5" :class="icon"></span>
+  </button>
+</template>
